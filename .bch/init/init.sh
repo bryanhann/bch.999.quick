f151c346a4530f62ff84d9c03b904d0ec73a59ee
Bryan Hann <nobody@nowhere>
2025-05-18 13:32:51 -0230

    Use [:::debug:::] to trace init.sh

--- .bch/init/init.sh
+++ .bch/init/init.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+:::debug::: ++[${BASH_SOURCE[0]}] [$@]
 for pth in $(ls $(dirname ${BASH_SOURCE[0]})/[0-9]*); do
     :::debug::: . $pth ${BASH_SOURCE[1]}
     . $pth ${BASH_SOURCE[1]}
 done
+:::debug::: ++[${BASH_SOURCE[0]}] [$@]
 
