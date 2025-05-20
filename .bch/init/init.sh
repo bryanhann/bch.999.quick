30c5ce381f1a6d16f6f328006f6511739d4757c9
Bryan Hann <nobody@nowhere>
2025-05-20 17:50:33 -0230

    Link scripts in [./local/bin/bch.quick.d]

--- .bch/init/init.sh
+++ .bch/init/init.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
-:::debug::: ++[${BASH_SOURCE[0]}] [$@]
+:::debug::: ++[${BASH_SOURCE[0]}]
+
 for pth in $(ls $(dirname ${BASH_SOURCE[0]})/[0-9]*); do
-    :::debug::: . $pth ${BASH_SOURCE[1]}
-    . $pth ${BASH_SOURCE[1]}
+    #. $pth ${BASH_SOURCE[1]}
+    :::debug::: . $pth
+    . $pth
 done
-:::debug::: ++[${BASH_SOURCE[0]}] [$@]
+
+:::debug::: --[${BASH_SOURCE[0]}]
 
