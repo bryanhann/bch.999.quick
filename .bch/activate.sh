d06b2dd4e8e5d099268e3b081b2702afe8c99351
Bryan Hann <nobody@nowhere>
2025-06-02 13:57:16 -0230

    Use [activate.sh] and [bch:000:*] function names

--- .bch/activate.sh
+++ .bch/activate.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
+
 export BCH_QUICK__root=$(dirname $(dirname ${BASH_SOURCE[0]}))
 export BCH_QUICK__bin=${BCH_QUICK__root}/.bch.bin
 export BCH_QUICK__lbin=${BCH_QUICK__root}/.bch.lbin
 export BCH_QUICK__files=${BCH_QUICK__root}/.bch.files
 export BCH_QUICK__init=${BCH_QUICK__root}/.bch/init/init.sh
 
-:::linkall:::  ${BCH_QUICK__lbin}
-
 . $(dirname ${BASH_SOURCE[0]})/init/init.sh
 
+bch:000:linkall  ${BCH_QUICK__lbin}
--- .bch/init/init.sh
+++ .bch/init/init.sh
@@ -1,12 +1,8 @@
 #!/usr/bin/env bash
 
-:::debug::: ++[${BASH_SOURCE[0]}]
-
 for pth in $(ls $(dirname ${BASH_SOURCE[0]})/[0-9]*); do
-    #. $pth ${BASH_SOURCE[1]}
-    :::debug::: . $pth
-    . $pth
+    bch:000:debug "        . $(basename $pth)"
+    source $pth
 done
 
-:::debug::: --[${BASH_SOURCE[0]}]
 
